1fb7276386236fae54fbd00b40e74bf52175204a
Biplob Adhikari <50793821+MrOxizen@users.noreply.github.com>
2020-02-13 13:11:45 +0600

    update

--- assets/frontend/js/touch.js
+++ assets/frontend/js/touch.js
@@ -1,14 +1,14 @@
 jQuery.noConflict();
 (function ($) {
     'use strict';
-    $('.oxi-image-hover-figure').on('touchstart', function (e) {
+    $('.oxi-image-hover').on('touchstart', function (e) {
         "use strict";
         var link = jQuery(this);
         if (link.hasClass("oxi-touch")) {
             return true;
         } else {
             link.addClass("oxi-touch");
-            $(".oxi-image-hover-figure").not(this).removeClass("oxi-touch");
+            $(".oxi-image-hover").not(this).removeClass("oxi-touch");
             e.preventDefault();
             return false;
         }
